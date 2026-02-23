87a932d1406613568c138dc96e1c44f7198a62e5
Rai <felicianohenry760@gmail.com>
2026-02-23 21:17:57 +0800

    First Animation

---
+++ animations/FIRST ANIMATE/script.js
@@ -0,0 +1,47 @@
+const container = document.querySelector('.container');
+const modal = document.querySelector('.modal');
+
+const open = document.getElementById('show');
+const close = document.getElementById('close');
+
+/* OPEN MODAL */
+open.addEventListener('click', () => {
+    container.classList.add('active');
+    modal.style.animation = "open 0.8s forwards";
+        currentIndex = 0;
+        text.innerText = originalText;
+        animateText();
+});
+
+/* CLOSE MODAL */
+close.addEventListener('click', () => {
+
+    const h1 = document.querySelector('.modal h1');
+    const p = document.querySelector('.modal p');
+    const button = document.querySelector('.modal button');
+
+    /* Fade text out */
+    h1.style.animation = "fadeOut 0.3s ";
+    p.style.animation = "fadeOut 0.3s ";
+    button.style.animation = "fadeOut 0.3s ";
+
+    /* Close width first */
+    modal.style.animation = "closeWidth 0.5s forwards";
+
+    /* Then close height */
+    setTimeout(() => {
+        modal.style.animation = "closeHeight 0.5s forwards";
+    }, 500);
+
+    /* Remove modal after animation */
+    setTimeout(() => {
+
+        container.classList.remove('active');
+
+        h1.style.animation = "showText 1s linear 2s forwards";
+        p.style.animation = "showText 1s linear 2s forwards";
+        button.style.animation = "showText 1s linear 2s forwards";
+
+    }, 1000);
+});
+
---
+++ animations/FIRST ANIMATE/script_2.js
@@ -0,0 +1,42 @@
+
+    const text = document.getElementById("text");
+    const letters = "abcdefghijklmnopqrstuvwxyz 0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?`~ ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const originalText = text.innerText;
+
+    let currentIndex = 0;
+
+    function animateText() {
+
+        if (currentIndex >= originalText.length +1) return;
+
+        let iteration = 0;
+
+        const interval = setInterval(() => {
+            
+
+            let displayText = "";
+
+            // Show already completed letters
+            displayText += originalText.substring(0, currentIndex);
+
+            // Show current animated letter (random)
+            displayText += letters[Math.floor(Math.random() * letters.length)];
+
+            // Hide remaining letters using blank spaces
+            displayText += " ".repeat( Math.max(0, originalText.length - currentIndex - 1));
+
+            text.innerText = displayText;
+
+            iteration++;
+
+            if (iteration >= 5) {
+                clearInterval(interval);
+                
+                // Lock the correct letter
+                currentIndex++;
+                animateText();
+            }
+
+        }, 10);
+    }
+    animateText();
